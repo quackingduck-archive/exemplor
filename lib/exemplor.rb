d0c1054395df4b5b3562502155132f8eeb81e60a
Myles Byrne <myles@ducknewmedia.com.au>
2009-11-21 11:56:47 +1100

    FANCY OUTPUT
    Without fancy:
    
      (s) version matches file:
        (s) version: |
          2000.2.0
      (s) errors are caught and nicely displayed:
        (s) result['status']: error
        (s) result['result']['class']: RuntimeError
        (s) result['result']['message']: boom!
        (s) result['result']['backtrace'][0]: examples/an_error.rb:4
      (s) check_output_matches_expected_for :no_checks:
        (s) output: |-
          - name: An example block without any checks prints the value of the block
            status: info (no checks)
            result: foo
      (s) check_output_matches_expected_for :oneliner:
        (s) output: |-
          - name: 1+2
            status: info (no checks)
            result: 3
      (s) check_output_matches_expected_for :no_checks_non_string:
        (s) output: |-
          - name: Non-string return values get converted to yaml
            status: info (no checks)
            result: !ruby/object:MyClass
              foo: bar
    
      ...etc
    
    With fancy:
    
      ✓ version matches file
      ✓ errors are caught and nicely displayed
      ✓ check_output_matches_expected_for :no_checks
      ✓ check_output_matches_expected_for :oneliner
      ✓ check_output_matches_expected_for :no_checks_non_string
    
    Essentially. The terminal output is pretty, uses ansi colors and is non-parsable, but when you call exemplor from something other than a terminal you're back to nice yaml

--- lib/exemplor.rb
+++ lib/exemplor.rb
@@ -1,12 +1,28 @@
 require 'orderedhash'
 require 'yaml'
 
+def OrderedHash(&blk)
+  ohsh = OrderedHash.new
+  blk.call(ohsh)
+  ohsh
+end
+
+def YAML.without_header(obj)
+  obj.to_yaml.match(/^--- \n?/).post_match
+end
+
+class String
+  def indent
+    self.split("\n").map { |line| '  ' + line }.join("\n")
+  end
+end
+
 module Exemplor
   
   def self.version() File.read(__FILE__.sub('lib/exemplor.rb','VERSION')) end
   
   class ExampleDefinitionError < StandardError ; end
-    
+  
   class Check
     
     attr_reader :expectation, :value
@@ -48,7 +64,67 @@ module Exemplor
     
   end
   
-  class Example
+  class ResultPrinter
+    
+    attr_reader :name,:status,:result,:stderr
+    
+    def initialize(name,status,result,stderr)
+      @name,@status,@result,@stderr = name,status,result,stderr
+    end
+    
+    def failure?
+      [:error,:failure].include?(self.status)
+    end
+    
+    def yaml
+      hsh = OrderedHash do |o|
+        o['name'] = self.name
+        o['status'] = case status = self.status
+          when :info  : 'info (no checks)'
+          when :infos : 'info (with checks)'
+          else ; status.to_s
+        end
+        o['result'] = self.result
+      end
+      YAML.without_header([hsh])# prints an array
+    end
+    
+    def fancy
+      # •∙ are inverted in my terminal font (Incosolata) so I'm swapping them
+      require 'term/ansicolor'
+      case status
+      when :info : blue "• #{name}\n" + YAML.without_header(result).indent
+      when :infos
+        formatted_result = result.map do |r|
+          "• #{r['name']}\n" + YAML.without_header(r['result']).indent
+        end.join("\n")
+        blue("∙ #{name}\n" + formatted_result.indent)
+      when :success
+        green("✓ #{name}")
+      when :failure
+        # sooo hacky
+        failure = result.find { |r| r['status'] == 'failure' }
+        out = failure.dup
+        out.delete('status')
+        out.delete('name')
+        color(:red,  "✗ #{name} - #{failure['name']}\n#{YAML.without_header(out).indent}")
+      when :error
+        class_and_message = "#{result['class']} - #{result['message']}"
+        backtrace = result['backtrace'].join("\n")
+        color(:red, "☠ #{name}\n#{class_and_message.indent}\n#{backtrace.indent}")
+      end
+    end
+    
+    def blue(str) color(:blue,str) end
+    def green(str) color(:green,str) end
+    
+    def color(color, str)
+      [Term::ANSIColor.send(color), str, Term::ANSIColor.reset].join
+    end
+    
+  end
+  
+  class ExampleEnv
     
     class << self
       
@@ -56,6 +132,66 @@ module Exemplor
       attr_accessor :setup_block
       
       def setup(&blk) self.setup_block = blk end
+      
+      # runs the block in the example environment, returns triple:
+      # [status, result, stderr]
+      def run(&code)
+        env = self.new
+        stderr = StringIO.new
+        status, result = begin
+          real_stderr = $stderr ; $stderr = stderr # swap stderr
+          
+          env.instance_eval(&self.setup_block) if self.setup_block
+          value = env.instance_eval(&code)
+          result = env._status == :info ? 
+            render_value(value) : render_checks(env._checks)
+          [env._status, result]
+          
+        rescue Object => error
+          [:error, render_error(error)]
+        ensure
+          $stderr = real_stderr # swap stderr back
+        end
+        [status, result, stderr.rewind && stderr.read]
+      end
+      
+      # -- these "render" methods could probably be factored away
+      
+      # yaml doesn't want to print a class
+      def render_value(value)
+        value.kind_of?(Class) ? value.inspect : value
+      end
+      
+      def render_checks(checks)
+        failure = nil
+        out = []
+        checks.each do |check|
+          failure = check if check.failure?
+          break if failure
+          out << OrderedHash do |o|
+            o['name'] = check.name
+            o['status'] = check.status.to_s
+            o['result'] = render_value check.value
+          end
+        end
+        if failure
+          out << OrderedHash do |o|
+            o['name'] = failure.name
+            o['status'] = failure.status.to_s
+            o['expected'] = failure.expectation
+            o['actual'] = render_value failure.value
+          end
+        end
+        out
+      end
+
+      def render_error(error)
+        OrderedHash do |o|
+          o['class'] = error.class.name
+          o['message'] = error.message
+          o['backtrace'] = error.backtrace
+        end
+      end
       
     end
     
@@ -74,37 +210,16 @@ module Exemplor
       check
     end
     
-  end
-  
-  class Result
-    
-    attr_accessor :name,:status,:result,:stderr
-    
-    def initialize(name,status,result,stderr)
-      @name,@status,@result,@stderr = name,status,result,stderr
-    end
-    
-    def failure?
-      [:error,:failure].include?(self.status)
-    end
-    
-    def print_yaml
-      hsh = OrderedHash.new
-      hsh['name'] = self.name
-      hsh['status'] = case status = self.status
-        when :info  : 'info (no checks)'
-        when :infos : 'info (with checks)'
-        else ; status.to_s
-      end
-      hsh['result'] = self.result
-      puts [hsh].to_yaml.match(/^--- \n/).post_match # prints an array
+    def _status
+      (:info    if _checks.empty?) ||
+      (:infos   if _checks.all? { |c| c.info? }) ||
+      (:success if _checks.all? { |c| c.success? }) ||
+      (:failure if _checks.any? { |c| c.failure? })
     end
     
   end
   
   class Examples
-    
-    attr_writer :setup_block
     
     def initialize
       @examples = OrderedHash.new
@@ -120,10 +235,10 @@ module Exemplor
       patterns = Regexp.new(patterns.join('|'))
       examples_to_run = @examples.select { |name,_| name =~ patterns }
       return 0 if examples_to_run.empty?
-      examples_to_run.each do |name, body|
-        result = Result.new(name, *run_example(body))
+      examples_to_run.each do |name, code|
+        result = ResultPrinter.new(name, *ExampleEnv.run(&code))
         fails +=1 if result.failure?
-        result.print_yaml
+        puts($stdout.tty? ? result.fancy : result.yaml)
       end
       (fails.to_f/examples_to_run.size)*100
     end
@@ -131,85 +246,7 @@ module Exemplor
     def list(patterns)
       patterns = Regexp.new(patterns.join('|'))
       list = @examples.keys.select { |name| name =~ patterns }
-      print_yaml list
-    end
-    
-    def print_yaml(obj)
-      out = obj.to_yaml.match(/^--- \n/).post_match
-      print(out)
-    end
-        
-    def run_example(code)
-      status = :info
-      env = Example.new
-      stderr = StringIO.new
-      out = begin
-        real_stderr = $stderr
-        $stderr = stderr
-        
-        env.instance_eval(&Example.setup_block) if Example.setup_block
-        value = env.instance_eval(&code)
-        if env._checks.empty?
-          render_value value
-        else
-          status = :infos if env._checks.all? { |check| check.info? }
-          status = :success if env._checks.all? { |check| check.success? }
-          status = :failure if env._checks.any? { |check| check.failure? } 
-          render_checks(env._checks)
-        end
-      rescue Object => error
-        status = :error
-        render_error(error)
-      ensure
-        $stderr = real_stderr
-      end
-      [status, out, stderr.rewind && stderr.read]
-    end
-    
-    def render_checks(checks)
-      failure = nil
-      out = []
-      checks.each do |check|
-        failure = check if check.failure?
-        break if failure
-        out << ohsh do |o|
-          o['name'] = check.name
-          o['status'] = check.status.to_s
-          o['result'] = render_value check.value
-        end
-      end
-      if failure
-        out << ohsh do |o|
-          o['name'] = failure.name
-          o['status'] = failure.status.to_s
-          o['expected'] = failure.expectation
-          o['actual'] = render_value failure.value
-        end
-      end
-      out
-    end
-    
-    def render_error(error)
-      out = OrderedHash.new
-      out['class'] = error.class.name
-      out['message'] = error.message
-      out['backtrace'] = error.backtrace
-      out
-    end
-    
-    def status_icon(status)      
-      icon = status == :infos ? '(I)' : "(#{status.to_s.slice(0,1)})"
-    end
-    
-    # yaml doesn't want to print a class
-    def render_value(value)
-      value.kind_of?(Class) ? value.inspect : value
-    end
-    
-    def ohsh(&blk)
-      ohsh = OrderedHash.new
-      blk[ohsh]
-      ohsh
+      print YAML.without_header(list)
     end
     
   end
@@ -224,8 +261,11 @@ module Exemplor
   
 end
 
+# Defines an example. After definition, an example is an entry in the
+# Examples.examples ordered hash, the key is the name, the body is the example
+# code
 def eg(name = nil, &example)
-  return Exemplor::Example if name.nil? && example.nil?
+  return Exemplor::ExampleEnv if name.nil? && example.nil?
   if name.nil?
      file, line_number = caller.first.match(/^(.+):(\d+)/).captures
      line = File.readlines(file)[line_number.to_i - 1].strip
@@ -235,6 +275,7 @@ def eg(name = nil, &example)
   Exemplor.examples.add(name, &example)
 end
 
+# Parses the command line args and either runs or lists the examples.
 at_exit do
   args = ARGV.dup
   if args.delete('--list') || args.delete('-l')
