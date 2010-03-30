4c1690f8e633e9624361c69a8a2e84a35ded5578
Myles Byrne <myles@myles.id.au>
2010-03-30 11:46:24 -0500

    better info

--- examples/assertion_success_and_failure.rb
+++ examples/assertion_success_and_failure.rb
@@ -4,7 +4,7 @@ eg 'Some successes, then a fail' do
   list = [1, 2, 3]
   Check(list.first).is(1)
   Check(list[1]).is(2)
-  Check(list.last).is(1)
+  Check(list.last).is(1) # fail!
   Check(list[2]).is(3) # would be successful but we never get here
 end
 
